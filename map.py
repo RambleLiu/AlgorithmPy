26a90d91b81ab2d8cd128db529878922d713738a
RambleLiu <1275467268@qq.com>
2020-05-17 23:52:29 +0800

    '邻接矩阵深度遍历算法'

--- map.py
+++ map.py
@@ -1,3 +1,5 @@
+import structure as stru
+
 # 最小生成树
 
 # 邻接矩阵规模
@@ -17,3 +19,51 @@ class AdjLink:
 
     # 顶点表,元素为edge
     vTable = []
+
+
+# 邻接矩阵的广度优先遍历
+# 针对无向图，可按照上三角压缩存储，故此只遍历上三角区域
+def bfs(data):
+    if data is None:
+        print('图不能为空！')
+        return None
+    # 结果队列
+    result = stru.Rqueue()
+
+    # 辅助队列
+    q = stru.Rqueue()
+    # 初始化辅助队列
+    q.enQueue(0)
+    while not q.isEmpty():
+        # 出队时访问节点
+        row = q.deQueue().data
+        result.enQueue(row)
+        # 遍历一层
+        for i in range(row + 1, MAX_SIZE):
+            if data.edge[row][i] > 0:
+                q.enQueue(i)
+    print(result)
+
+
+# 邻接矩阵的深度优先遍历
+# 由于是无向图，因此采用可以采用压缩存储的遍历方式
+def dfs(data):
+    if data is None:
+        print('图不能为空！')
+        return None
+    # 结果队列
+    result = stru.Rqueue()
+
+    # 辅助栈
+    stack = stru.Rstack()
+
+    # 初始化辅助栈
+    stack.push(0)
+    while not stack.isEmpty():
+        row = stack.pop().data
+        result.enQueue(row)
+        for i in range(row + 1, MAX_SIZE):
+            # 入栈时访问节点
+            if data.edge[row][i] > 0:
+                stack.push(i)
+    print(result)
--- structure.py
+++ structure.py
@@ -1,18 +1,17 @@
-# 链式队列结点
-class Node:
-    data = None
-    next = None
+# 链式队列
+class Rqueue:
+    # 链式队列结点
+    class Node:
+        data = None
+        next = None
 
-
-# 队列
-class Queue:
     front = Node()
     rear = front
     length = 0
 
     # 入队
     def enQueue(self, data):
-        node = Node()
+        node = self.Node()
         node.data = data
         self.rear.next = node
         self.rear = node
@@ -42,3 +41,39 @@ class Queue:
             rs += str(index.data) + ','
             index = index.next
         return rs[0:-1]
+
+
+# 链式栈
+class Rstack:
+    class Node:
+        data = None
+        pre = None
+
+    # 带有头结点，使出栈、入栈操作一致
+    top = Node()
+    length = 0
+
+    # 出栈
+    def pop(self):
+        if self.length <= 0:
+            print('空栈不支持pop操作！')
+            return None
+        rs = self.top
+        self.top = self.top.pre
+        self.length -= 1
+        return rs
+
+    # 入栈
+    def push(self, data):
+        node = self.Node()
+        node.data = data
+        node.pre = self.top
+        self.top = node
+        self.length += 1
+
+    # 队列判空
+    def isEmpty(self):
+        if self.length > 0:
+            return 0
+        else:
+            return 1
